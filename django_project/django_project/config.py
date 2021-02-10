74e2268d0ee8af6c49beacd1784b6de93f5b7159
Bekzod Buyukov <bek.buyukov@gmail.com>
2021-02-10 16:18:44 +0500

    Small corrections

--- django_project/django_project/config.py
+++ django_project/django_project/config.py
@@ -1,12 +1,12 @@
 import environ
 
 
-# # making env
+# making env
 env = environ.Env()
 
 # reading .env file
 env.read_env()
 
-#  # getting values
+# getting values
 DEBUG = env('DEBUG', default=True)
-SECRET_KEY = env('SECRET_KEY', default="NEED SECRET KEY")
+SECRET_KEY = env('SECRET_KEY', default='NEED SECRET KEY')
